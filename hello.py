5876b0f17e78174cf6dfad5736df4c493c409cef
sanjpand <56039971+sanjpand@users.noreply.github.com>
2020-10-04 20:31:53 +0100

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  3 15:29:01 2020
+
+@author: sanjee
+"""
+
+
+print("Content-Type: text/plain")
+print("")
+print("Congratulations, it's a web app!")
